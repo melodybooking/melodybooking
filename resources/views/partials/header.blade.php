0ee6efb6d61dcad7410cf4c84dcd3be679e01aff
John Watts <johnwatts@gmx.com>
2017-05-09 18:11:13 -0500

    Removed extra button type from Submit button to enable Login

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -237,7 +237,7 @@
 
                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
 
-                       <button type="button" class="btn btn-primary" type="submit">Submit</button>
+                       <button class="btn btn-primary" type="submit">Submit</button>
 
                    </div>
 
