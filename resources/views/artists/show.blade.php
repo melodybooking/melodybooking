e1e7573fc0430b016f13068655669383f4cd2dca
Joshua Williams <joshuamw322@gmail.com>
2017-05-15 15:33:24 -0500

    changed layouts on view pages for edit user, create user, user edit password, and the user show blade

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -2,19 +2,38 @@
 
 @section('content')
 
-	<h3>{{ $artist->artist_name }}</h3>
-	<article class= "col-md-4">
-		<p>{{ $artist->bio }}</p>
-		<p></p>
+	<div class="container-fluid" id="artistShow">
 
-		@if (Auth::id() == $artist->created_by)
-	 	<a class="btn btn-primary" href="{{ action('PostsController@edit', $artist->id) }}">Edit</a>
-		@endif
-	</article>
+		<article class= "col-md-12">
 
-	<div class= "col-md-4">
-		<img src = "/uploads/images/{{ $artist->image }}" class="img-responsive" id="showItemImage" alt="Image">
+			<h3>{{ $artist->artist_name }}</h3>
+
+			<p>{{ $artist->bio }}</p>
+
+			<p><strong>Genre:</strong> {{ $artist->genre}}</p>
+
+			<div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+				<img src = "/uploads/images/{{ $artist->image }}" class="img-responsive" id="showItemImage" alt="Image">
+
+			</div>
+
+			<div class ="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+				<h5>Contact Info</h5>
+
+				<p>{{ $artist->email }}</p>
+		
+				@if (Auth::id() == $artist->created_by)
+
+		 			<a class="btn btn-primary" href="{{ action('PostsController@edit', $artist->id) }}">Edit Artist Profile</a>
+
+				@endif
+
+			</div>
+
+		</article>
+
 	</div>
-
 
 @stop
