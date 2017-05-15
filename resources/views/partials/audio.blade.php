73025bb8ef3e3a2bc4ca47cd2bb984662a4454d2
IssaJeran <jeransmith22@gmail.com>
2017-05-15 18:11:10 -0500

    Added audio player js, view, mp3s etc

---
+++ resources/views/partials/audio.blade.php
@@ -0,0 +1,48 @@
+<h5>Enjoy a Freshly Curated Playlist by MelodyBooking</h5>
+
+<div class="player">
+
+<div class="pl"></div>
+
+<div class="title"></div>
+
+<div class="artist"></div>
+
+<!-- <div class="cover"></div> -->
+
+<div class="controls">
+
+<div class="play"></div>
+
+<div class="pause"></div>
+
+<div class="rew"></div>
+
+<div class="fwd"></div>
+
+</div>
+
+<div class="volume"></div>
+
+<div class="tracker"></div>
+
+</div>
+
+<ul class="playlist hidden">
+
+<li audiourl="nananana.mp3" cover="cover1.jpg">Bobby Tables</li>
+
+<li audiourl="nodudelatestreetnights.mp3" cover="cover2.jpg">Bobby Tables</li>
+
+<li audiourl="blaze.mp3" cover="cover2.jpg">Bobby Tables</li>
+
+<li audiourl="STOPthebloodflow.mp3" cover="cover2.jpg">Bobby Tables</li>
+
+<li audiourl="sadnesswithyoshiguitar.mp3" cover="cover2.jpg">Bobby Tables</li>
+
+
+<!-- <li audiourl="06.mp3" cover="cover6.jpg" artist="Artist 6">06.mp3</li>
+
+<li audiourl="07.mp3" cover="cover7.jpg" artist="Artist 7">07.mp3</li> --> --> -->
+
+</ul>
