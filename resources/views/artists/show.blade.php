2a4a972d9fa67f86e0ad34a7b7aef5b6729f7231
John Watts <johnwatts@gmx.com>
2017-05-16 15:47:53 -0500

    Show contact and edit buttons only if logged in or artist creator

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -2,79 +2,84 @@
 
 @section('content')
 
+<div class="container-fluid" id="artistShow">
 
-	<div class="container-fluid" id="artistShow">
+	<article class= "col-md-12">
 
-		<article class= "col-md-12">
+		<h3>{{ $artist->artist_name }}</h3>
 
-			<h3>{{ $artist->artist_name }}</h3>
+		<p>{{ $artist->bio }}</p>
 
-			<p>{{ $artist->bio }}</p>
+		<p><strong>Genre:</strong> {{ $artist->genre}}</p>
 
-			<p><strong>Genre:</strong> {{ $artist->genre}}</p>
+		<div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
-			<div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+			<img src = "/uploads/images/{{ $artist->image }}" class="img-responsive" id="showItemImage" alt="Image">
 
-				<img src = "/uploads/images/{{ $artist->image }}" class="img-responsive" id="showItemImage" alt="Image">
-
-			</div>
+		</div>
 
 
-	<!-- Button trigger modal -->
-	<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Contact Artist</button>
+<!-- Button trigger modal -->
+	@if (Auth::check())
+<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Contact Artist</button>
+	@endif
 
-			<div class ="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<div class ="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
-				@if (Auth::id() == $artist->created_by)
+			@if (Auth::id() == $artist->created_by)
 
-		 			<a class="btn btn-primary" href="{{ action('PostsController@edit', $artist->id) }}">Edit Artist Profile</a>
+				<a class="btn btn-primary" href="{{ action('PostsController@edit', $artist->id) }}">Edit Artist Profile</a>
 
-				@endif
+			@endif
 
-			</div>
+		</div>
 
-		</article>
+	</article>
 
+</div>
+
+<!-- Modal -->
+@if (Auth::check())
+<div class="modal fade slide left" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+	<div class="modal-content">
+	  <div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+
+		</button>
+
+		<h1 class="modal-title" id="myModalLabel">Contact Artist</h1>
+
+	  </div>
+	  <div class="modal-body">
+		<p class="lead">Please get in touch for bookings!</p>
+		<form method="POST" id="myForm" action="{{ action('PostsController@sendMail') }}">
+
+			{!! csrf_field() !!}
+
+		  <div class="form-group">
+			<label for="name">Your name:</label>
+			<input type="text" name="name" id="name" class="form-control" placeholder="Name" value="{{ Auth::user()->name }}" required/>
+		  </div>
+		  <div class="form-group">
+			<label for="email">Your email:</label>
+			<input type="email" name="email" id="email" class="form-control" placeholder="Email" value="{{ Auth::user()->email }}" required/>
+		  </div>
+		  <div class="form-group">
+			<label for="text">Your message:</label>
+			<textarea class="form-control" id="text" name="text" required></textarea>
+		  </div>
+		  <input type="hidden" name="id" value="{{ $artist->id }}">
+		  <input type="submit" name="submit" class="btn btn-success btn-lg" value="submit">
+		</form>
+	  </div>
+	  <div class="modal-footer">
+		<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel Form</button>
+	  </div>
 	</div>
-
-	<!-- Modal -->
-	<div class="modal fade slide left" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-
-	        </button>
-	        <h1 class="modal-title" id="myModalLabel">Contact Artist</h1>
-
-	      </div>
-	      <div class="modal-body">
-	        <p class="lead">Please get in touch for bookings!</p>
-	        <form method="POST" id="myForm" action="{{ action('PostsController@sendMail') }}">
-
-				{!! csrf_field() !!}
-
-	          <div class="form-group">
-	            <label for="name">Your name:</label>
-	            <input type="text" name="name" id="name" class="form-control" placeholder="Name" value="{{ Auth::user()->name }}" required/>
-	          </div>
-	          <div class="form-group">
-	            <label for="email">Your email:</label>
-	            <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="{{ Auth::user()->email }}" required/>
-	          </div>
-	          <div class="form-group">
-	            <label for="text">Your message:</label>
-	            <textarea class="form-control" id="text" name="text" required></textarea>
-	          </div>
-			  <input type="hidden" name="id" value="{{ $artist->id }}">
-	          <input type="submit" name="submit" class="btn btn-success btn-lg" value="submit">
-	        </form>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel Form</button>
-	      </div>
-	    </div>
+</div>
 	</div>
+@endif
 
 
 @stop
