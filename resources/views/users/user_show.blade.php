39135fd3464995ec1aadabce9f67182e50a2d438
John Watts <johnwatts@gmx.com>
2017-05-17 11:32:51 -0500

    Added user's artists to User show blade

--- resources/views/users/user_show.blade.php
+++ resources/views/users/user_show.blade.php
@@ -8,10 +8,22 @@
 
 		<p><strong>Email:</strong> {{ $user->email }}</p>
 
+
+<div class="container">
+	<div class="row" id="itemsPage">
+		@foreach($artist as $artist)
+		<article class= "col-md-4">
+			<h3><a href="{{ action('PostsController@show', $artist->id) }}">{{ $artist->artist_name }}</a></h3>
+			<p><img src = "/uploads/images/{{ $artist->image }}" class="img-responsive" id="showArtistImage" alt="Image"></p>
+		</article>
+		@endforeach
+	</div>
+</div>
+
 		<form action="{{ action('UserController@edit', \Auth::id() ) }}" method="GET">
 
 			{!!csrf_field()!!}
-			
+
 			<input type="submit" class="btn btn-primary" value="Edit User Account Information" style="margin-top: 5px; margin-bottom: 5%;">
 
 		</form>
