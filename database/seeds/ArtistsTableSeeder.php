3511f4d4685efe03e7a44e540c9c8812e7159d15
John Watts <johnwatts@gmx.com>
2017-05-21 16:36:21 -0500

    Added artist's locations to all artists in database

--- database/seeds/ArtistsTableSeeder.php
+++ database/seeds/ArtistsTableSeeder.php
@@ -18,7 +18,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
   	   $artist->artist_name = 'Red Cardinal';
   	   $artist->email = 'joshuamw322@gmail.com';
   	   $artist->bio = '';
- 	   $artist->location = 'San Antonio';
+ 	   $artist->location = 'San Antonio, TX';
   	   $artist->genre = 'Rock';
   	   $artist->facebook_url = 'https://www.facebook.com/redcardinalsatx';
   	   $artist->instagram_url = 'https://www.instagram.com/redcardinalsatx/';
@@ -33,7 +33,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->artist_name = 'TAYLOR';
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Born and raised in San Antonio, TX, Taylor is poised to continue her dominant run in the modeling industry and farther pursue her passion in music. An incredibly talented individual in all creative areas. She is certain to make any event a success.';
-	   $artist->location = 'San Antonio';
+	   $artist->location = 'San Antonio, TX';
  	   $artist->genre = 'Pop, Model';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = '';
@@ -50,7 +50,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Best Producer DOA';
  	   $artist->genre = 'Rap, Experimental';
-	   $artist->location = 'San Antonio';
+	   $artist->location = 'San Antonio, TX';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = '';
  	   $artist->twitter_url = '';
@@ -65,6 +65,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'zaytovenbookings@gmail.com';
  	   $artist->bio = 'Xavier L. Dotson, professionally known as Zaytoven, is an American record producer, DJ, and pianist from Atlanta, Georgia. He is known for his work with Southern rap artists Gucci Mane and Migos, and has released joint projects with artists such as Future, Waka Flaka Flame, Young Scooter, Bankroll Fresh, and Young Dro.';
  	   $artist->genre = 'Rap, Producer';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/zaytovenbeatz/';
  	   $artist->twitter_url = 'https://twitter.com/zaytovenbeatz/';
@@ -79,6 +80,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Internationally based musician, model and designer with a different take on the traditional design palette. Using multiple mediums to create whimsical yet aesthetically pleasing designs that you wouldn\'t find in store. All designs are one of kind custom pieces made by hand that are sure to make heads turn just like her music';
  	   $artist->genre = 'Pop, Model, Designer';
+	   $artist->location = 'International';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/cydnieg/';
  	   $artist->twitter_url = '';
@@ -93,6 +95,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'lyorcohen@300ent.com';
  	   $artist->bio = 'Jeffery Lamar Williams (born August 16, 1991), best known professionally as Young Thug, is an American hip hop artist from Atlanta, Georgia. Known for his unconventional vocal style, fashion, and persona, he first received attention for his collaborations with fellow Southern rappers, such as Rich Homie Quan, Birdman, T.I., and Gucci Mane. Young Thug initially released a series of independent mixtapes beginning in 2011 with I Came From Nothing. In early 2013, he signed with Gucci Mane\'s 1017 Records and later that year he released his label debut mixtape 1017 Thug to critical praise. Young Thug received mainstream recognition in 2014 with the songs "Stoner" and "Danny Glover" in addition to appearances on several singles, including "About the Money" and "Lifestyle." That year, he also signed to Lyor Cohen\'s 300 Entertainment and collaborated on the mixtapes Black Portland and Rich Gang: Tha Tour Pt. 1. In 2015, he released a number of mixtapes, including Barter 6 and two installments of his Slime Season series. These were followed in 2016 by the mixtapes I\'m Up, Slime Season 3, and Jeffery in anticipation of his long-delayed debut studio album.';
  	   $artist->genre = 'Rap, Experimental';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = 'https://www.facebook.com/youngthugmusic/';
  	   $artist->instagram_url = 'https://www.instagram.com/thuggerthugger1/';
  	   $artist->twitter_url = 'https://twitter.com/youngthug';
@@ -107,6 +110,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'nesslybooking@gmail.com';
  	   $artist->bio = 'Atlanta native Nessly has thrived in the SoundCloud rap scene for some time, garnering cosigns from other influential artists and OVO Sound Radio in the process.';
  	   $artist->genre = 'Rap, Experimental';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/nessly/';
  	   $artist->twitter_url = 'https://twitter.com/nessly24k';
@@ -121,6 +125,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Producer out of Austin, TX with an extensive resume and prolific work ethic combined with an extremely unique sound making future waves.';
  	   $artist->genre = 'Rap, Experimental';
+	   $artist->location = 'Austin, TX';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/yung_yoshi/';
  	   $artist->twitter_url = 'https://twitter.com/nobueats';
@@ -134,6 +139,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->artist_name = 'BOOU';
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Producer out of Austin, TX';
+	   $artist->location = 'Austin, TX';
  	   $artist->genre = 'Rap, Experimental';
  	   $artist->facebook_url = 'https://www.facebook.com/BoouMusic/';
  	   $artist->instagram_url = 'https://www.instagram.com/booumusic/';
@@ -149,6 +155,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Born and raised in Atlanta, Bankreaux is prepared to take over the rap game growing a steady local fanbase currently out of Austin, TX.';
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/bankreaux/';
  	   $artist->twitter_url = '';
@@ -163,6 +170,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'wheezywaves@gmail.com';
  	   $artist->bio = 'Wheezy tends toward the spacious and spare; his beats give Young Thug room, so that their songs are as much shaped by Thug\'s vocals as any drums. In a serenely stoned conversation over the phone from Atlanta, Wheezy spoke about finding chemistry with Thug, and his Atlanta mentors. He is also currently working with Future, Spooky Black, Travis Scott, and many more.';
  	   $artist->genre = 'Rap, Producer';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = '';
  	   $artist->twitter_url = 'https://twitter.com/wheezy5th/';
@@ -177,6 +185,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'lyorcohen@300ent.com';
  	   $artist->bio = 'Rapper signed with Young Thug\'s label YSL under 300 entertainment' ;
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Unknown';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/only1_gunna/';
  	   $artist->twitter_url = 'https://twitter.com/1gunnagunna?lang=en';
@@ -191,6 +200,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'lyorcohen@300ent.com';
  	   $artist->bio = 'Shayaa Bin Abraham-Joseph (born October 22, 1992), known professionally as 21 Savage, is an American hip hop recording artist from Atlanta, Georgia. He is best known for his mixtapes, The Slaughter Tape and Slaughter King and his extended plays Free Guwop and Savage Mode with Metro Boomin.';
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = 'https://www.facebook.com/21Savage/';
  	   $artist->instagram_url = 'https://www.instagram.com/21savage/';
  	   $artist->twitter_url = 'https://twitter.com/21savage';
@@ -205,6 +215,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'rickyracksbooking@gmail.com';
  	   $artist->bio = 'Ricky Racks, whose real name is Ricky Harrell, is a 30 year old producer hailing from Greenville, North Carolina. He grew up in a musical family, learning a lot from his father who played guitar and his mother who used to sing in a quartet group. Growing up, Ricky learned how to play the drums at the age of six. Ricky Racks co-produced one of Young Thug’s biggest hits, “Best Friend,” and has consistently worked with him on other tracks during his career as a producer. In 2017, Ricky Racks has produced on the tracks “What the Price” by Migos and “Peek a Boo” by Lil Yachty.';
  	   $artist->genre = 'Rap, Producer';
+	   $artist->location = 'Greenville, NC';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/rickyracksicu/';
  	   $artist->twitter_url = 'https://twitter.com/rickyracksicu';
@@ -219,6 +230,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'smithtalent.mgmt@gmail.com';
  	   $artist->bio = 'Jerome Fortune (born October 13, 1988), better known by his stage name Rome Fortune, is an American hip hop recording artist from Atlanta, Georgia who rose to prominence after the release of his Beautiful Pimp mixtape in 2013. Influenced by a diverse range of musical traditions, Fortune has released 6 EPs and has collaborated with a number of artists from the Atlanta hip hop scene, including OG Maco, Dun Deal, and ILoveMakonnen. He is currently working on his much anticipated Beautiful Pimp 3 EP.';
  	   $artist->genre = 'Rap, Experimental';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = 'https://www.facebook.com/RomeFortune/';
  	   $artist->instagram_url = 'https://www.instagram.com/romefortune/';
  	   $artist->twitter_url = 'https://twitter.com/romefortune';
@@ -233,6 +245,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'THOUXANBANMGMT@GMAIL.COM';
  	   $artist->bio = 'Rapper Thouxanbanfauni, from Chattanooga, TN, but raised In Atlanta, is associated with Playboi Carti & UnoTheActivist.';
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Chattanooga, TN';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/thouxbanfauni/';
  	   $artist->twitter_url = 'https://twitter.com/Thouxanbanfauni';
@@ -247,6 +260,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = '2900ent@gmail.com';
  	   $artist->bio = 'UnoTheActivist is an up-and-coming rapper from Zone 3, Atlanta. He released his debut mixtape, No More Thotties, in late 2015, which was followed up by Gift of Gab, a mixtape which was released in early 2016. Some of Uno’s most common collaborators are: Maxo Kream, Playboi Carti, which is also his cousin, & ThouxanbandFauni.';
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Atlanta, GA';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/unotheactivist/';
  	   $artist->twitter_url = 'https://twitter.com/unotheactivist';
@@ -261,6 +275,7 @@ class ArtistsTableSeeder extends DatabaseSeeder
  	   $artist->email = 'lyorcohen@300ent.com';
  	   $artist->bio = 'Duke is a member of YSL – Young Slime Life/Young Stoner Life with Young Thug. Duke’s twitter name is YSLDuke, He has a few projects including Lil Duke, Uber, and new mixtape Blue Devil — Uber is his third mixtape, his first two mixtapes were the self-titled Lil Duke and Hit Hard Move Silent.';
  	   $artist->genre = 'Rap';
+	   $artist->location = 'Unknown';
  	   $artist->facebook_url = '';
  	   $artist->instagram_url = 'https://www.instagram.com/LilDuke60/';
  	   $artist->twitter_url = 'https://twitter.com/YslDuke';
